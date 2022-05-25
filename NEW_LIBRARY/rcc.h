37ac9b607cf45a0e561556e894aa810d3f3f52a0
Gabriele Felaco <8165980+Gabroxs@users.noreply.github.com>
2022-05-25 20:14:49 +0200

    Add files via upload

---
+++ NEW_LIBRARY/rcc.h
@@ -0,0 +1,18 @@
+#ifndef RCC_H
+#define RCC_H
+
+void RCC_GPIOA(unsigned int enable);
+void RCC_GPIOB(unsigned int enable);
+void RCC_GPIOC(unsigned int enable);
+void RCC_GPIOD(unsigned int enable);
+void RCC_GPIOE(unsigned int enable);
+void RCC_GPIOF(unsigned int enable);
+void RCC_DMA1(unsigned int enable);
+void RCC_DMA2(unsigned int enable);
+void RCC_ADC12(unsigned int enable);
+void RCC_ADC34(unsigned int enable);
+void RCC_TIM1(unsigned int enable);
+void RCC_TIM2(unsigned int enable);
+void RCC_TIM3(unsigned int enable);
+
+#endif
