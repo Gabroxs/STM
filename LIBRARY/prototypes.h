d1e13f7914d4aa582042051dcac82e44974f0eea
Gabriele Felaco <8165980+Gabroxs@users.noreply.github.com>
2022-05-25 12:04:09 +0200

    Add files via upload

--- LIBRARY/prototypes.h
+++ LIBRARY/prototypes.h
@@ -24,5 +24,13 @@
    void sine3Wave(unsigned int *const vector, unsigned int size);
    void squareWave(unsigned int *const vector, unsigned int size);
    void triangularWave(unsigned int *const vector, unsigned int size);
-
+  
+   
+   void ADC1_start(void);
+   void ADC1_conversion_length(unsigned int length);
+   void ADC12_clk_domain(unsigned int ckmode);
+   void ADC_difsel(unsigned int difsel, unsigned int channel);
+   void ADC1_calibration(unsigned int adcaldif);
+   void ADC1_enable(unsigned int aden);
+   void ADC1_vreg_en(void);
 #endif
